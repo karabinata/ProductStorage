c7aa343ba0eac7907ade1ee6c93ebf231cbf265c
karabinata <karabinata1992@gmail.com>
2017-04-14 15:23:15 +0300

    Skeleton

---
+++ ProductStorage.Data/ProductStorageContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ProductStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.Data
+{
+    public class ProductStorageContext : IdentityDbContext<ApplicationUser>
+    {
+        public ProductStorageContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ProductStorageContext Create()
+        {
+            return new ProductStorageContext();
+        }
+    }
+}
