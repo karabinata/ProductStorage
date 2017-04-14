e30cb176f39680e95a1aa1b023c298abcdaadc4e
karabinata <karabinata1992@gmail.com>
2017-04-14 16:46:46 +0300

    some other changes

--- ProductStorage/ViewModels/Account/RegisterViewModel.cs
+++ ProductStorage/ViewModels/Account/RegisterViewModel.cs
@@ -23,5 +23,10 @@ namespace ProductStorage.ViewModels.Account
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        public HttpPostedFileBase ProfilePicture { get; set; }
     }
 }
